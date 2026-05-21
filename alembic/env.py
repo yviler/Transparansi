38bb321b29755504f58b4f381dbb9d693d5ceed0
yviler <muhazzriel@gmail.com>
2026-05-21 20:27:44 +0700

    alembic script for wallets and projects

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.database import Base
 from app.models.users import Users
+from app.models.projects import Projects
+from app.models.wallets import Wallets
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
