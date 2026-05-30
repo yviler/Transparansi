af8ba56720a4580f2f3e9cfb234e7ab7517db8fd
yviler <muhazzriel@gmail.com>
2026-05-31 00:04:02 +0700

    refactor: enhance session token management, improve flash message handling, and style form elements

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -4,7 +4,10 @@ from app.database import get_db, AsyncSession
 from sqlalchemy import select
 from datetime import datetime, timezone, timedelta
 
-async def insertSessionToken(user: Users, 
+from app.utils import flash
+
+
+async def insertSessionToken(user: Users,
                              token: str, db: AsyncSession) -> None:
     user.session_token = token
     user.session_token_expires_at = datetime.now(timezone.utc) + timedelta(minutes=30)
@@ -17,12 +20,12 @@ async def verifySession(request: Request,
                         db: AsyncSession = Depends(get_db)) -> Users:
     session_id = request.cookies.get("session_id")
 
-    #if session_id is None, it will return user where session_token is Null
     user = (await db.execute(select(Users).where(Users.session_token == session_id))).scalars().first()
 
     
     #create HTTPException handler for template response
     if not session_id or not user or user.session_token_expires_at < datetime.now(timezone.utc):
+        # delete session_token and session_token_expires_at and logout
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid or expired session. Please login"
@@ -37,8 +40,20 @@ async def currentUser(request: Request,
     
     if not session_id:
         return None
+
     user = (await db.execute(select(Users).where(Users.session_token == session_id))).scalars().first()
+
+    if user is None:
+        return None
     
+    elif user.session_token_expires_at < datetime.now(timezone.utc):
+        user.session_token = None
+        user.session_token_expires_at = None
+        db.add(user)
+        await db.commit()
+        flash.flash(request, "session token has expired, please log in", "error")
+        return None
+
     return user
         
 # dependency for role checking
