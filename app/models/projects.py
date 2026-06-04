8dc719e8169d75b416f3206750b0ebea71dba16c
yviler <muhazzriel@gmail.com>
2026-06-04 15:04:25 +0700

    add tasks and task assignees class, alembic awaiting

--- app/models/projects.py
+++ app/models/projects.py
@@ -15,3 +15,26 @@ class Projects(Base):
     wallet_id = Column(UUID, ForeignKey("wallets.id"), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
     finished_at = Column(TIMESTAMP(timezone=True), nullable=True)   
+    
+class Tasks(Base):
+    __tablename__ = " tasks"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    task_name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    projectID = Column(UUID, ForeignKey("projects.id"), nullable=False, index=True)
+    parentTaskID = Column(UUID, ForeignKey("tasks.id"), nullable=True, index=True)
+    status = Column(Enum('pending', 'ongoing', 'completed', 'cancelled', name='task_status'), nullable=False, default='pending')
+    # should be a list of UUID instead, (many to one) for users.id
+    assignees = Column(UUID, ForeignKey("users.id"), nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    finished_at = Column(TIMESTAMP(timezone=True), nullable=True)
+    
+    
+# what if we want to remove a user, i dont want deletes, make revoked = bool default false
+class TaskAssignees(Base):
+    __tablename__ = "task_assignees"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, index=True, default=uuid.uuid4)
+    task_id = Column(UUID, ForeignKey("tasks.id"), nullable=False, index=True)
+    user_id = Column(UUID, ForeignKey("users.id"), nullable=False, index=True)
